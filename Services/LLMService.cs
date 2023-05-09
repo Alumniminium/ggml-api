c016e9fdc6a3bb518b50e6c0f62f801dddd3b06c
trbl <domi@outlook.at>
2023-05-09 13:46:20 +0200

    fix busywait and increase keepalive timeout to 10min

--- Services/LLMService.cs
+++ Services/LLMService.cs
@@ -69,6 +69,9 @@ public class LLMService : ILLMService
 
         if (dto.includeStatistics)
             yield return GetStatistics();
+        
+        if (dto.oneShot)
+            LLM.ClearContext();
 
         LLM.Busy = false;
     }
